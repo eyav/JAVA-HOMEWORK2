ceb3d8191751b88e0b1221f3ae8417686e9ead85
ERDENER YAVUZ <80373319+eyav@users.noreply.github.com>
2022-09-28 18:50:12 +0300

    Add files via upload

---
+++ reCapDemo_classes/src/recapdemo_classes/ReCapDemo_classes.java
@@ -0,0 +1,11 @@
+package recapdemo_classes;
+
+public class ReCapDemo_classes {
+
+    public static void main(String[] args) {
+        DortIslem dortIslem=new DortIslem();
+        int sonuc=dortIslem.topla(5, 0);
+        System.out.println(sonuc);
+    }
+
+}
